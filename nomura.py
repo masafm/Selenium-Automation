9be27a194e4920720a78157d674bb1829a8b9c7c
Masafumi Kashiwagi <mkashi3@gmail.com>
2020-02-14 00:43:28 +0900

    野村證券IPO申し込み対応

--- nomura.py
+++ nomura.py
@@ -10,19 +10,33 @@ try:
     ipo.driver.find_element_by_id("passwd1").send_keys(ipo.decrypt(ipo.args.password))
     ipo.driver.find_element_by_name("buttonLogin").click()
     try:
+        # 最初のお知らせスキップ
         ipo.driver.implicitly_wait(3)
         ipo.driver.find_element_by_name("buttonTop").click()
         ipo.driver.implicitly_wait(30)
     except:
         pass
-    ipo.driver.find_element_by_link_text("取引").click()
-    ipo.driver.find_element_by_link_text("IPO/PO").click()
-    ipo.driver.implicitly_wait(1)
-    try:
-        ipo.driver.find_element_by_xpath("//p[contains(.,'対象明細はありません。')]").click()
-    except:
-        ipo.driver.implicitly_wait(30)
-        print("野村證券IPOがある場合の処理", file=sys.stderr)
+    
+    for i in range(5):
+        ipo.driver.find_element_by_link_text("取引").click()
+        ipo.driver.find_element_by_link_text("IPO/PO").click()
+        try:
+            ipo.driver.find_element_by_xpath("//a[contains(text(), '抽選申込へ')]").click();
+        except:
+            break
+        checkboxes = ipo.driver.find_elements_by_xpath("//input[@type='checkbox']")
+        for c in checkboxes:
+            c.click()
+        ipo.driver.find_element_by_xpath("//button[@name='buttonAgree']").click()
+        ipo.driver.find_element_by_class_name("apl-js-cmspsp").click()
+        ipo.driver.find_element_by_xpath("//button[@name='buttonAgree']").click()
+        ipo.driver.find_element_by_xpath("//button[@name='buttonConfirm']").click()
+        ipo.driver.find_element_by_name("trhkPswd").send_keys(ipo.decrypt(ipo.args.password2))
+        ipo.screenshot()
+        ipo.driver.find_element_by_xpath("//button[@name='buttonAccept']").click()
+        ipo.screenshot()
+        print("野村証券IPO("+str(i)+")に申し込みました", file=sys.stderr)
+        
     ipo.driver.quit()
 except:
     ipo.exit_on_err()
