e04a8d4aa442ac287335b5e9b7be7952f3c5e7a8
Masafumi Kashiwagi <mkashi3@gmail.com>
2020-02-19 21:41:07 +0900

    みずほ証券修正

--- mizuho.py
+++ mizuho.py
@@ -18,24 +18,19 @@ try:
             ipo.driver.find_element_by_xpath("(//table[@class='tblMod02 mgt10t'])[1]//a[contains(text(),'申込')]").click()
         except:
             break
-        ipo.driver.implicitly_wait(5)
-        for j in range(5):
-            ipo.driver.find_element_by_name("send").click()
-            try:
-                pdf = ipo.driver.find_elements_by_xpath("//a[contains(@href,'.pdf')]")
-                for p in pdf:
-                    p.click()
-            except:
-                pass
-            try:
-                ipo.driver.find_element_by_name("kabuSuFeedbackPanelParent:mousikomiKabusuu:base:_value")
-                break
-            except:
-                pass
-        ipo.driver.implicitly_wait(30)
+        ipo.driver.find_element_by_xpath("//input[@value='次へ']").click()
+        ipo.driver.find_element_by_xpath("//input[@value='同意する']").click()
+        try:
+            ipo.driver.find_element_by_xpath("//input[@value='電子交付を承認する']").click()
+            pdf = ipo.driver.find_elements_by_xpath("//a[contains(@href,'.pdf')]")
+            for p in pdf:
+                p.click()
+            ipo.driver.find_element_by_xpath("//input[@value='内容を理解した']").click()
+        except:
+            pass
         ipo.driver.find_element_by_name("kabuSuFeedbackPanelParent:mousikomiKabusuu:base:_value").send_keys('1')
         ipo.driver.find_element_by_name("kakakuFeedbackPanelParent:nedanRadioChoice").click()
-        ipo.driver.find_element_by_name("send").click()
+        ipo.driver.find_element_by_xpath("//input[@value='申込確認画面へ']").click()
         ipo.driver.implicitly_wait(5)
         try:
             ipo.driver.find_element_by_name("ansyouBangouFeedbackPanelParent:ansyouBangou").send_keys(ipo.decrypt(ipo.args.password2))
@@ -44,11 +39,11 @@ try:
             ipo.driver.find_element_by_name("kabuSuFeedbackPanelParent:mousikomiKabusuu:base:_value").clear()
             ipo.driver.find_element_by_name("kabuSuFeedbackPanelParent:mousikomiKabusuu:base:_value").send_keys('100')
             ipo.driver.find_element_by_name("kakakuFeedbackPanelParent:nedanRadioChoice").click()
-            ipo.driver.find_element_by_name("send").click()
+            ipo.driver.find_element_by_xpath("//input[@value='申込確認画面へ']").click()
             ipo.driver.find_element_by_name("ansyouBangouFeedbackPanelParent:ansyouBangou").send_keys(ipo.decrypt(ipo.args.password2))
         ipo.driver.implicitly_wait(30)
         ipo.screenshot()
-        ipo.driver.find_element_by_name("tyuusenSanka").click()        
+        ipo.driver.find_element_by_xpath("//input[@value='説明を理解し抽選参加する']").click()
         ipo.screenshot()
         print("みずほ証券IPO("+str(i)+")に申し込みました", file=sys.stderr)
         
