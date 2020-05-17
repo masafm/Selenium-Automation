efc5d34b8b7767bb35b40c6d5d20aad6e2a93e68
Masafumi Kashiwagi <mkashi3@gmail.com>
2020-05-17 13:13:31 +0900

    Fix a bug

--- pairs.py
+++ pairs.py
@@ -36,22 +36,25 @@ try:
         time.sleep(3)
 
     a.driver.get('https://pairs.lv/')
-    for i in range(1,10):
-        a.driver.get('https://pairs.lv/#/search/grid/'+str(i))
-        time.sleep(3)
-        likes = a.driver.find_elements_by_xpath("//p[@class='user_like_count user_like_count_a']")
-        buttons = a.driver.find_elements_by_xpath("//ul[@class='list_view_users']/..//span[.='いいね！']")
-        for j in range(16):
-            if int(likes[j].text.replace('+','')) > 90 and int(likes[j].text.replace('+','')) < 300:
-                action = webdriver.common.action_chains.ActionChains(a.driver)
-                action.move_to_element_with_offset(buttons[j], 5, 5)
-                action.click()
-                action.perform()
-                try:
-                    a.driver.find_element_by_xpath("//p[contains(text(), '「いいね！」を送りますか？')]/..//a[text()='いいね！']").click()
-                except:
-                    a.driver.find_element_by_xpath("//div[@id='like_question_answer_modal']//img").click()
-                time.sleep(3)
+    try:
+        for i in range(1,10):
+            a.driver.get('https://pairs.lv/#/search/grid/'+str(i))
+            time.sleep(3)
+            likes = a.driver.find_elements_by_xpath("//p[@class='user_like_count user_like_count_a']")
+            buttons = a.driver.find_elements_by_xpath("//ul[@class='list_view_users']/..//span[.='いいね！']")
+            for j in range(16):
+                if int(likes[j].text.replace('+','')) > 90 and int(likes[j].text.replace('+','')) < 300:
+                    action = webdriver.common.action_chains.ActionChains(a.driver)
+                    action.move_to_element_with_offset(buttons[j], 5, 5)
+                    action.click()
+                    action.perform()
+                    try:
+                        a.driver.find_element_by_xpath("//p[contains(text(), '「いいね！」を送りますか？')]/..//a[text()='いいね！']").click()
+                    except:
+                        a.driver.find_element_by_xpath("//div[@id='like_question_answer_modal']//img").click()
+                        time.sleep(3)
+    except:
+        pass
     a.driver.quit()
 except:
     a.exit_on_err()
