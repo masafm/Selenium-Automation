44bc57eb31463f85f5d2cfe26fd0705708754dd8
Masafumi Kashiwagi <mkashi3@gmail.com>
2020-07-05 17:33:31 +0900

    SPAT4精算処理修正

--- rakuten_spat4.py
+++ rakuten_spat4.py
@@ -16,7 +16,7 @@ try:
     a.driver.find_element_by_xpath("//input[@value='精算指示確認へ']").click()
     a.driver.find_element_by_id("MEMBERPASSR").send_keys(a.decrypt(a.args.password2))
     a.driver.find_element_by_xpath("//input[@value='精算指示する']").click()
-    time.sleep(5)
+    time.sleep(30)
 except:
     pass
 
